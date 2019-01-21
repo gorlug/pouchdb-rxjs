6219b5c4a55af0903c5cb96b7d61e22ce5da0c8a
Achim Rohn <achim@rohn.eu>
2019-01-21 22:09:14 +0100

    Export PouchDBDocumentList

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./lib/Logger";
 export * from "./lib/CouchDBWrapper";
 export * from "./lib/PouchDBDocument";
 export * from "./lib/PouchDBWrapper";
+export * from "./lib/PouchDBDocumentList";
